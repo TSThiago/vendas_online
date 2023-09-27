1ba2fcb02781966745d7d3395f4a6576d727d32c
Thiago Shibanuma <thiago.tshibanuma@gmail.com>
2023-09-27 20:43:46 -0300

    criação do input

---
+++ src/modules/login/styles/login.style.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components/native";
+
+export const ContainerLogin = styled.View`
+    height: 100%;
+    width: 100%;
+    justify-content: center;
+    align-items: center;
+    background-color: pink;
+    padding: 16px;
+`;
