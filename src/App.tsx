5a7f8cfb9396685b4737029a93ae584e5d276da4
Thiago Shibanuma <thiago.tshibanuma@gmail.com>
2023-09-27 19:58:48 -0300

    criação da screen de login

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import Login from './modules/login/screens/Login';
+
+export default function App() {
+  return (
+    <View>
+      <Login/>
+    </View>
+  );
+}
+
